e02257066743080867c052dd9407b533334f4ff9
John Martin <jtm3@g.clemson.edu>
2016-08-30 17:49:04 -0400

    Exploratory Data Analysis
    Playing around with PCA, Logistic, and neural nets in R with some (arbitrarily chosen) variables

---
+++ exploratory.R
@@ -0,0 +1,56 @@
+library(psych)
+library(pROC)
+library(caret)
+library(e1071)
+library(nnet)
+
+full.data <- read.csv("~/2005-2013.csv")
+full.data$Site.Dummy <- ifelse(full.data$Site == "Home", 1, 0)
+
+#WILL BE WRONG ON NEUTRAL FIELDS
+full.data$Pct.Capacity <- full.data$Attendance / full.data$Capacity
+
+games <- full.data[, !names(full.data) %in% c("Date", "Game.Code", "Visit.Team.Code", 
+                                              "Home.Team.Code", "Stadium.Code", "Site",
+                                              "Duration", "Stadium", "Attendance", "City",
+                                              "State", "Capacity", "Surface", "Year.Opened",
+                                              "Home.Team.Name", "Home.Conference.Code", 
+                                              "Away.Team.Name", "Away.Conference.Code",
+                                              "Home.Conference", "Home.Subdivision",
+                                              "Away.Conference", "Away.Subdivision",
+                                              "Home.Points", "Away")]#,
+                                              #"Home.Win")] 
+games.pca <- prcomp(games,
+                 center = TRUE,
+                 scale. = TRUE)
+
+plot(games.pca, type = "l")
+
+model <- glm(Home.Win ~ Home.Pass.Yard + Home.Rush.Yard + Home.Sack
+             + Home.Time.Of.Possession
+             + Home.Tackle.Solo + Home.Tackle.Assist + Home.Kickoff,family=binomial(link='logit'),data=games)
+summary(model)
+prob <- predict(model,Home.Win=c(1))
+games$prob <- prob
+
+g <- roc(Home.Win ~ prob, data = games)
+plot(g)  
+
+coords(g, "best")
+
+games$prediction <- ifelse(prob > 0.5,1,0)
+
+confusionMatrix(games$prediction,games$Home.Win, positive = '1')
+
+net <- nnet(Home.Win ~ Home.Pass.Yard + Home.Rush.Yard + Home.Sack
+                 + Home.Penalty.Yard + Home.Time.Of.Possession
+                 + Home.Tackle.Solo + Home.Tackle.Assist + Home.Kickoff, data = games, size = 10)
+summary(net)
+
+net.prob <- predict(net, Home.Win=c(1))
+games$net.prob=net.prob
+
+games$net.prediction <- ifelse(net.prob > 0.5,1,0)
+
+#Was working...isn't now. Why?
+confusionMatrix(games$net.prediction, games$Home.Win, positive = '1')
