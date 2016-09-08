ffe3f6e35a99adaac749fe5bd94aa66ff0981bb4
John Martin <jtm3@g.clemson.edu>
2016-09-08 17:11:00 -0400

    Add files via upload

--- Exploratory Data Analysis.R
+++ Exploratory Data Analysis.R
@@ -5,6 +5,8 @@ library(TTR)
 library(magrittr)
 library(randomForest)
 library(caTools)
+library(flexclust)
+library(ggplot2)
 
 games.mav <- read.csv("~/games mav.csv")
 
@@ -19,10 +21,25 @@ train <- games.mav[x,]
 test <- games.mav[!x,]
 
 #LOGISTIC REGRESSION
-model <- glm(Win ~ Pass.Yard + Rush.Yard + Sack.Yard + Time.Of.Possession + Tackle.Solo 
-             + Tackle.Assist + Kickoff.Yard,family=binomial(link='logit'),data=train)
+model <- glm(train$Win ~ .
+               #Pass.Yard + Rush.Yard + Sack.Yard + Time.Of.Possession + Tackle.Solo 
+             #+ Tackle.Assist + Kickoff.Yard
+             ,family=binomial(link='logit'),data=train[,11:75])
+
+model <- step(model,scope=formula(model),
+     direction="backward",k=2)  
+
+#BACKWARD STEPWISE CHOSE THIS
+model <- glm(train$Win ~ Rush.Yard + Rush.TD + Pass.TD + Kickoff.Ret + Kickoff.Ret.TD + 
+  Punt.Ret.TD + Fum.Ret.TD + Int.Ret + Int.Ret.TD + Misc.Ret.Yard + 
+  Off.XP.Kick.Att + Off.XP.Kick.Made + Off.2XP.Att + Def.2XP.Att + 
+  Safety + Punt.Yard + Kickoff.Yard + Tackle.Solo + Tackle.Assist + 
+  Sack + Fumble.Forced + X1st.Down.Rush + X1st.Down.Penalty + 
+  Time.Of.Possession + Penalty + Penalty.Yard + Fourth.Down.Att
+  ,family=binomial(link='logit'),data=train[,11:75])
+
 summary(model)
-prob <- predict(model, test[,11:50])
+prob <- predict(model, test[,11:75])
 test$prob <- as.numeric(prob)
 
 g <- roc(Win ~ prob, data = test)
@@ -63,3 +80,23 @@ forest.prediction <- predict(forest, test[,11:75])
 test$forest.prediction <- forest.prediction
 
 confusionMatrix(test$forest.prediction, test$Win, positive = '1')
+
+
+#K MEANS CLUSTERING
+set.seed(420)
+
+games.cluster <- kmeans(train[,11:75], 2, nstart = 20)
+summary(games.cluster)
+
+games.cluster$cluster <- as.factor(games.cluster$cluster)
+ggplot(train[,11:75], aes(Pass.TD + Rush.TD, Sack, color = games.cluster$cluster)) + geom_point()
+
+games.cluster <- as.kcca(games.cluster, data=train[,11:75])
+
+
+#Mess Around with linear regression
+l <- lm(Pass.TD + Rush.TD ~ . + train$Time.Of.Possession, train[,11:25])
+summary(l)
+
+
+
