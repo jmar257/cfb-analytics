faa7db889bcf7f57709a9bc0764661df660f1288
John Martin <jtm3@g.clemson.edu>
2016-09-13 16:47:08 -0400

    Add files via upload

--- Elo.R
+++ Elo.R
@@ -1,44 +1,30 @@
 library(PlayerRatings)
 library(dplyr)
+library(plyr)
 
-# games <- read.csv("~/2005-2013 Unique Game.csv")
-# 
-# games.for.elo <- games[,c("Date", "Home.Team.Code", "Visit.Team.Code", "Home.Win")]
-# 
-# games.for.elo$Date <- as.numeric(as.POSIXlt(games.for.elo$Date, format="%m/%d/%Y"))
-# 
-# games.elo <- elo(games.for.elo, init = 1000, k = 15, sort = TRUE)
+games <- read.csv("~/2005-2013 Unique Game Weeks.csv")
+teams <- read.csv("~/team.csv")
 
-drives <- read.csv("~/2005-2013 Drives.csv")
-drives$End.Reason <- as.factor(drives$End.Reason)
-# end.reason <- drives$End.Reason
-# dummies <- model.matrix(~drives$End.Reason)
-# drives <- cbind(drives, dummies)
-# drives$End.Reason <- end.reason
+games <- merge(games, teams[,1:2], by.x="Home.Team.Code", by.y="Team.Code")
+games <- rename(games, c("Name" = "Home.Name"))
+games <- merge(games, teams[,1:2], by.x="Visit.Team.Code", by.y="Team.Code")
+games <- rename(games, c("Name" = "Away.Name"))
 
-drives$Points <- ifelse(drives$End.Reason == "TOUCHDOWN", 7, 
-                    ifelse(drives$End.Reason == "FIELD GOAL", 3, 
-                           ifelse(drives$End.Reason == "SAFETY", -2, 0)))
-
-drives$Start.Spot <- as.factor(drives$Start.Spot)
-
-expected <- aggregate(drives$Points, by = list(drives$Start.Spot), mean)
-
-expected$Group.1 <- (100 - as.numeric(expected$Group.1))
-
-drives$Starting.Pos.To.EZ <- (100 - as.numeric(drives$Start.Spot))
-
-colnames(expected) <- c("Starting.Pos.To.EZ", "Expected.Points")
-
-drives <- merge(drives, expected, by="Starting.Pos.To.EZ")
-
-l <- lm(Points ~ as.numeric(Starting.Pos.To.EZ), drives)
-summary(l)
+games$Home.Name <- as.character(games$Home.Name)
+games$Away.Name <- as.character(games$Away.Name)
 
 
-plot(expected, type = "c", xlab = "Yards to End Zone", ylab = "Expected Points")
-lines(expected)
-abline(l)
+games.for.elo <- games[,c("Unique.Week", "Home.Name", "Away.Name", "Home.Win")]
 
-write.csv(drives, file = "~/Drives with Expected Points.csv", row.names = FALSE)
+#games.for.elo$Date <- as.numeric(as.POSIXlt(games.for.elo$Date, format="%m/%d/%Y"))
 
+games.elo <- elo(games.for.elo, init = 1500, k = 25, sort = TRUE, history = TRUE)
+ratings <- games.elo$ratings
+history <- as.data.frame(t(as.data.frame(games.elo$history)))
+history <- history[1:137, ]
+
+team <- "Clemson"
+
+plot(x = 1:length(history[, team]), y = history[, team], type='c', xlab = team,
+     ylab = 'Elo Rating')
+lines(history[, team])
---
+++ Expected Points.R
@@ -0,0 +1,32 @@
+drives <- read.csv("~/2005-2013 Drives.csv")
+drives$End.Reason <- as.factor(drives$End.Reason)
+# end.reason <- drives$End.Reason
+# dummies <- model.matrix(~drives$End.Reason)
+# drives <- cbind(drives, dummies)
+# drives$End.Reason <- end.reason
+
+drives$Points <- ifelse(drives$End.Reason == "TOUCHDOWN", 7, 
+                        ifelse(drives$End.Reason == "FIELD GOAL", 3, 
+                               ifelse(drives$End.Reason == "SAFETY", -2, 0)))
+
+drives$Start.Spot <- as.factor(drives$Start.Spot)
+
+expected <- aggregate(drives$Points, by = list(drives$Start.Spot), mean)
+
+expected$Group.1 <- (100 - as.numeric(expected$Group.1))
+
+drives$Starting.Pos.To.EZ <- (100 - as.numeric(drives$Start.Spot))
+
+colnames(expected) <- c("Starting.Pos.To.EZ", "Expected.Points")
+
+drives <- merge(drives, expected, by="Starting.Pos.To.EZ")
+
+l <- lm(Points ~ as.numeric(Starting.Pos.To.EZ), drives)
+summary(l)
+
+
+plot(expected, type = "c", xlab = "Yards to End Zone", ylab = "Expected Points")
+lines(expected)
+abline(l)
+
+write.csv(drives, file = "~/Drives with Expected Points.csv", row.names = FALSE)
