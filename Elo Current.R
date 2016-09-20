f3934eb24ed7bd97b8087dddfcacc2124843a1a5
John Martin <jtm3@g.clemson.edu>
2016-09-20 16:02:52 -0400

    Update Elo Current.R

--- Elo Current.R
+++ Elo Current.R
@@ -14,33 +14,37 @@ games.for.elo <- games[,c("Unique.Week", "Home.Team", "Away.Team", "Home.Win")]
 numteams <- length(unique(c(games$Home.Team, games$Away.Team)))
 numweeks <- length(unique(games$Unique.Week))
 
-#numweeks <- 219
+numweeks <- 221
 
-games.for.elo <- games.for.elo[games.for.elo$Unique.Week < numweeks,]
+games.for.elo <- games.for.elo[games.for.elo$Unique.Week <= numweeks,]
 
-games.elo <- elo(games.for.elo, init = 1300, k = 23, sort = TRUE, history = TRUE)
+games.elo <- elo(games.for.elo, init = 1500, k = 25, sort = TRUE, history = TRUE)
 ratings <- games.elo$ratings
 history <- as.data.frame(t(as.data.frame(games.elo$history)))
 history <- history[1:(numweeks-1), ]
 
-hist(ratings$Rating, breaks = 4, xlab = "Elo Rating", ylab = "Frequency", main = "Distribution of Ratings")
+hist(ratings$Rating, breaks = 9, xlab = "Elo Rating", ylab = "Frequency", main = "Distribution of Ratings")
 #curve(dnorm(mean = 1500),add=T,col="blue")
 
-team <- "Alabama"
-team2 <- "Ohio State"
-weeks <- unique(games$Unique.Week)
-years <- merge(x = as.data.frame(weeks), y = games[,2:3], by.x = "weeks",
-               by.y = "Unique.Week", all.x = TRUE)
-years <- subset(years, !duplicated(weeks))
+team <- "Louisville"
+team2 <- "Florida State"
+#weeks <- unique(games$Unique.Week)
+# years <- merge(x = as.data.frame(weeks), y = games[,2:5], by.x = "weeks",
+#                by.y = "Unique.Week", all.x = TRUE)
+# years <- years[years$Day == "Sat.",]
+# years <- subset(years, !duplicated(weeks))
+# years$Date <- gsub("Sept.", "Sep.", years$Date)
+# years$Date <- as.Date(years$Date, "%b. %d, %Y")
                 
+plot(x = 2:numweeks, y = NULL, type='l', xlab = team,
+     ylab = 'Elo Rating',  xlim=c(1, numweeks),ylim=c(1150, 2050)) 
+#axis(1, at = 1:219, labels = format(years$Date, "%Y"), cex.axis = 1)
 
-plot(x = years$Season[2:221], y = history[, team], type='c', xlab = team,
-     ylab = 'Elo Rating', xlim=c(1, numweeks), ylim=c(1150, 2050))
+#ggplot( data = as.data.frame(cbind(years$Date[2:221], history[, team])), aes( V1, V2 )) + geom_line() 
 
 # Graph every team
 # for(i in 1:numteams) {
 #   lines(history[, i])
-# 
 
 lines(1:numweeks, rep(1500, numweeks), col = "black")
 lines(history[, team], col = "red")
@@ -52,10 +56,12 @@ team.score <- ratings[ratings$Player == team, "Rating"]
 team2.score <- ratings[ratings$Player == team2, "Rating"]
 m <- (team2.score - team.score) / 400
 prob.a <- 1 / (1 + (10^m))
+print(team.score)
+print(team2.score)
 prob.a
 
 
-
+#write.csv(history, "~/historical elo.csv", row.names = FALSE)
 
 
 
